568d8d14f8b1b6e8a8d4440f8fa1a0371ffe9f6f
Pedro Brandao <pcrbrandao@gmail.com>
2017-01-18 19:33:29 -0200

    Incluindo controller vazio, por enquanto

---
+++ CoreDataList/CoreDataList/CoreDataController.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataController.h
+//  CoreDataList
+//
+//  Created by Pedro Brandão on 18/01/17.
+//  Copyright © 2017 Pedro Brandão. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataController : NSObject
+
+@end
---
+++ CoreDataList/CoreDataList/CoreDataController.m
@@ -0,0 +1,13 @@
+//
+//  CoreDataController.m
+//  CoreDataList
+//
+//  Created by Pedro Brandão on 18/01/17.
+//  Copyright © 2017 Pedro Brandão. All rights reserved.
+//
+
+#import "CoreDataController.h"
+
+@implementation CoreDataController
+
+@end
